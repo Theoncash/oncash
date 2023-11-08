93331f9f0daf6c99a50e076c58b530d1db270b0a
AffanShaikhsurab <affanshaikhsurabofficial@gmail.com>
2023-11-08 18:19:50 +0530

    Caution ** contains issues in socialProof Features Added -> Firebase analytics for collecting revenue -> SocialProof( Contains bugs )

--- app/src/main/java/in/oncash/oncash/Repository/RoomRepo.kt
+++ app/src/main/java/in/oncash/oncash/Repository/RoomRepo.kt
@@ -1,6 +1,7 @@
 package `in`.oncash.oncash.Repository
 
 import android.content.Context
+import android.widget.Toast
 import `in`.oncash.oncash.Component.jsonConversion
 import `in`.oncash.oncash.DataType.FieldsX
 import `in`.oncash.oncash.DataType.withdrawalTransaction
@@ -13,39 +14,60 @@ import org.json.JSONArray
 
 class RoomRepo {
 
-    suspend fun getTransaction(context: Context): ArrayList<WithdrawalRequestEntity> {
-     return   withContext(Dispatchers.IO) {
+    suspend fun getTransaction(context: Context): ArrayList<WithdrawalRequestEntity> = withContext(Dispatchers.IO) {
          var withdrawRequestsJson: JSONArray;
                try {
                    withdrawRequestsJson   =
                        UserInfo_Airtable_Repo().getAllWithdrawTransaction()!!
+
+
+                   val withdrawRequests: ArrayList<FieldsX> =
+                       jsonConversion().convertJSON(withdrawRequestsJson)
+                   var withdrawRequestRoom: ArrayList<WithdrawalRequestEntity> = ArrayList()
+                   val database = withdrawRequestDB.getDatabase(context)
+
+                   for (request in withdrawRequests) {
+                  withContext(Dispatchers.Main){
+                      Toast.makeText(
+                          context,
+                          request.UserNumber.toString(),
+                          Toast.LENGTH_SHORT
+                      ).show()
+                  }
+
+                       // Get an instance of the Room database
+                       val withdrawalRequest = WithdrawalRequestEntity(
+                           UserNumber = request.UserNumber,
+                           WalletBalance = request.WalletBalance,
+                           Status = "Success",
+                           isDisplayed = false
+                       )
+                       database.withdrawalRequestDao().insertWithdrawalRequest(
+                           withdrawalRequest
+                       )
+
+                   }
+                   val d =   database.withdrawalRequestDao().getAllWithdrawalRequests().value
+                   if(d != null){
+                       withdrawRequestRoom = d as  ArrayList<WithdrawalRequestEntity>
+                   }
+                   return@withContext withdrawRequestRoom
                }catch (e:NullPointerException){
                    withdrawRequestsJson   =
                        UserInfo_Airtable_Repo().getAllWithdrawTransaction()!!
+                val data : ArrayList<WithdrawalRequestEntity> = ArrayList<WithdrawalRequestEntity>(
+                )
+                   data.add(   WithdrawalRequestEntity(
+                       UserNumber = 2132131123,
+                       WalletBalance = 0,
+                       Status = "Success",
+                       isDisplayed = true
+                   ))
+                   return@withContext  data
                }
-                val withdrawRequests: ArrayList<FieldsX> =
-                    jsonConversion().convertJSON(withdrawRequestsJson)
-                var withdrawRequestRoom: ArrayList<WithdrawalRequestEntity> = ArrayList()
-                val database = withdrawRequestDB.getDatabase(context)
 
-                for (request in withdrawRequests) {
-                    // Get an instance of the Room database
-                    val withdrawalRequest = WithdrawalRequestEntity(
-                        UserNumber = request.UserNumber,
-                        WalletBalance = request.WalletBalance,
-                        Status = "Success",
-                        isDisplayed = false
-                    )
-                    database.withdrawalRequestDao().insertWithdrawalRequest(
-                        withdrawalRequest
-                    )
-
-                }
-                withdrawRequestRoom = database.withdrawalRequestDao().getAllWithdrawalRequests().value as ArrayList<WithdrawalRequestEntity>
-
-                return@withContext withdrawRequestRoom
         }
-    }
+
 
     suspend fun updateWithdrawRequest(request : WithdrawalRequestEntity , context: Context) {
         val database = withdrawRequestDB.getDatabase(context)
